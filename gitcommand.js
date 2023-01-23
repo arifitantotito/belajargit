ac0f81a45b64370fc9a2f823da4e96eaea3e0fe4
Arifitanto Oktarian Hafayer <arifitantotito@Arifitantos-MacBook-Air.local>
2023-01-23 11:48:06 +0700

    Update git command

--- gitcommand.js
+++ gitcommand.js
@@ -1,3 +1,4 @@
+// INITIATION
 // 1. git init (untuk membaca seluruh kodingan) (Warning: UNTUK FIRST INITIATE)
 // 2. git add index.js / git add . (untuk mengirim index.js / keseluruhan)
 // 3. git commit -m "Create variable inside index.js" (untuk memberi pesan komit)
@@ -5,4 +6,12 @@
 // 5. git remote add origin https://github.com/arifitantotito/belajargit.git (warning: UNTUK FIRST INITIATE)
 // 6. git push origin master/master (push ke branch master)
 
+// CREATE NEW BRANCH
 // 1. git checkout -b Day-01 (membuat branch baru)
+// 2. git add .
+// 3. git commit -m ""
+// git push origin Day-01
+
+// CHANGE TO OTHER BRANCH
+// 1. git checkout main (masuk ke main)
+// 2. git pull origin main (mengambil main)
